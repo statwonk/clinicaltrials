87f0788ae3ba7b0b4374f40ad7a1073d87d62646
statwonk <cpeter9@gmail.com>
2015-08-06 09:37:24 -0500

    Fitting a parametric model, a Beta distribution in a way that respects censoring and then comparing to the real data.

--- pvalues.R
+++ pvalues.R
@@ -80,7 +80,6 @@ p_zoom
 # " < 0.05" are turned in to a range like: [0, 0.05]. Exact values
 # are treated like [0.05, 0.05], and left-censored values are treated
 # like [0.05, 1] ( > 0.05).
-
 d <- d %>%
   mutate(# simple case, p-values are numeric
     left = as.numeric(P_VALUE),
@@ -114,72 +113,77 @@ d <- d %>%
                    ), right)) %>%
   # there are a handful of values that fall outside of values that actually
   # fall outside the allowed range of p-values (probabilities must range
-  # from 0 to 1). ** I'll simply throw those away. **
-  filter(left >= 0 & left <= 1,
-         right >= 0 & right <= 1)
+  # from 0 to 1).
+  # ** I'll simply throw those away. **
+  # Have a better idea? Drop me a line.
+  # Also, notice that I throw away values of
+  # 0 and 1. We're about to fit a Beta
+  # distribution, which does not have support
+  # inclusive of 0 or 1. I'm guessing this is
+  # part of the reason that fitdistcens
+  # won't fit a vector with those values.
+filter(left > 0 & left < 1,
+       right > 0 & right < 1)
 
-tryCatch({
-  fitdistcens(
-    as.data.frame( # fitdistcense requires data.frame object
-      d[, c("left", "right")]),
-    "beta",
-    start = list(shape1 = 1,
-                 shape2 = 3),
-    gr = "BFGS", # tried working with optim to see
-    method = "BFGS", # if I could get it to optimize
-    lower = 0, # no luck.
-    upper = 1)
-},
-finally = print(
-  "Errors because apparently
-   fitdistcens (or optim) can't
-   handle beta values of 0 or 1??"))
+d <- as.data.frame(d)
 
-d <- d %>%
-  # throwing away values of 0 or 1.
-  filter(left > 0 & left < 1,
-         right > 0 & right < 1)
-
-censfit <- fitdistcens(
-  as.data.frame( # fitdistcense requires data.frame object
-    d[, c("left", "right")]),
+# I highly recommend the fitdistplus
+# package. It's incredibly useful for
+# parametric fitting.
+fit <- fitdistcens(
+  d[, c("left", "right")],
   "beta",
-  start = list(shape1 = 1,
-               shape2 = 3))
+  start = list(shape1 = 1, shape2 = 3))
 
-d <- d %>%
-  filter(left == right) %>%
-  select(left)
+# A look at the fit.
+fit
 
-fit <- fitdist(data = d$left, "beta")
-ppcomp(fit,
-       xlim = c(0, 0.06),
-       ylim = c(0, 0.06))
-ppcomp(fit)
+# Not a bad fit! However, p-values don't follow a
+# theoretical distribution. Under the null, p-values
+# are uniform(0, 1) distributed, but no such theory
+# underpins the alternative. The final product,
+# published p-values are a mixture of a uniform(0, 1)
+# distribution and some other distribution that represents
+# how successful scientists are at leveraging their prior
+# knowledge / information for designing and carrying out
+# experiments.
+plot(fit)
+# Also, notice that there aren't any major discontinuities
+# visible here. The fit is nice, but it's not perfect ---
+# probably because theory can't yet underpin this mixture
+# well. Tell me if I'm wrong! I don't think I am.
 
-b_fit <- bootdist(fit,
-                  niter = 10)
-b_cens <- bootdistcens(censfit,
-                       niter = 10)
-
-ggplot() +
-  geom_density(
+parametric_comparison_plot <- ggplot() +
+  xlab("Raw numeric p-values") +
+  geom_histogram(
+    data = d,
+    aes(x = left),
+    binwidth = 0.001,
+    fill = 'orange') +
+  geom_histogram(
     data = data.frame(
       x = rbeta(length(d$left),
-                shape1 = median(b_fit$estim$shape1),
-                shape2= median(b_fit$estim$shape2))),
+                shape1 = fit$estimate[[1]],
+                shape2= fit$estimate[[2]])),
     aes(x = x),
-    colour = "blue") +
-  geom_density(
-    data = data.frame(
-      x = rbeta(length(d$left),
-                shape1 = median(b_cens$estim$shape1),
-                shape2= median(b_cens$estim$shape2))),
-    aes(x = x),
-    colour = "green") +
-  geom_density(
-    data = data.frame(
-      x = d$left),
-    aes(x = x),
-    colour = "red")
+    binwidth = 0.001) +
+  theme_bw(base_size = 25)
+
+# This parametric fit is interesting to
+# look at for data nerds, but it's
+# fruit from a poison branch --- one
+# half of the p-value mixture has some
+# unknown distribution. For example, is
+# it unreasonable to round your p-value like
+# 0.01, 0.02, 0.03 like below? I don't think so.
+# Approximation is an important aspect of doing
+# good data analysis and communication.
+parametric_comparison_plot
+
+# Again, _interesting_. Tempting to point to
+parametric_comparison_plot +
+  coord_cartesian(xlim = c(0, 0.1)) +
+  annotate(
+    geom = "text", x = 0.04, y = 500,
+    label = "This difference could just be well-informed hypotheses.")
 
